98792cdec927d52fc8bcbd8fcff54f886e814383
ManarAli97 <a190@alamani.iq>
2022-03-16 21:40:38 +0300

    2020/08/15

---
+++ js/style.js
@@ -0,0 +1,11 @@
+/*global $ */
+
+$(function () {
+	'use strict';
+	$('.info-list li').click(function () {
+		$(this).addClass('active').siblings('li').removeClass('active');
+		//window.console.log($(this).data('class'));
+		$('.info-content div').hide();
+		$('.' + $(this).data('class')).fadeIn();
+   });
+});
